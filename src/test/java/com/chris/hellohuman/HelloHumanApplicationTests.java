37ff09ec0b44cd2dc7e80993843c6ff0f7da463a
christiantoves <christiano.toves@gmail.com>
2021-08-11 15:01:27 -0700

    first commit

---
+++ src/test/java/com/chris/hellohuman/HelloHumanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.hellohuman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloHumanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
